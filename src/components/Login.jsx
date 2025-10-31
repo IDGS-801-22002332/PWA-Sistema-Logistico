30eafb05486f832298486ac4595be50fb869b4df
IDGS-801-22002332 <jesusisaguirr@gmail.com>
2025-10-30 23:04:16 -0600

    Correcion 4

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Truck, Mail, Lock, LogIn } from 'lucide-react';
-import './login.css';
+import './Login.css';
 
 const Login = () => {
     const [email, setEmail] = useState('');
